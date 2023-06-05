8ca9cf19c14814fa878f2ebbd0f66a664186f7c9
smiy <79819956+smiyy@users.noreply.github.com>
2023-06-04 23:56:25 -0700

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,19 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+  {
+      section: "S/V Singles",
+    },
+    {
+      name: "[Gen 9] SPB",
+      desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
+      threads: [
+        `&bullet; <a href="https://www.smogon.com/forums/threads/3712619/">Random Battle Suggestions</a>`,
+      ],
+
+      mod: 'ssb',
+      team: 'randomStaffBros',
+      ruleset: ['Dynamax Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod'],
+    },
+];
